55ee7e85fad39b632220ba311a507290f4fdeb03
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2024-09-14 01:20:22 +0600

    14-09-24

--- mafft_snp_command.py
+++ mafft_snp_command.py
@@ -18,3 +18,9 @@ for file in /Users/khandker_shahed/Documents/vibrio_anguillarum/output_fasta2/al
 do mafft --auto "$file" > "/Users/khandker_shahed/Documents/Work with Onu Vai/Piscirickettsia salmonis/output_fasta/aligned/${file%.fasta}_aligned.fasta"; done
 
 mafft --auto hcp2.fasta > hcp2_aligned.fasta
+
+for file in /Users/khandker_shahed/Documents/vibrio_anguillarum/suppli_table/*.fasta; do mafft --auto "$file" > "/Users/khandker_shahed/Documents/vibrio_anguillarum/suppli_table/aligned/${file%.fasta}_aligned.fasta"; done
+
+mafft --auto tet34.fasta > tet34_aligned.fasta
+
+for file in /Users/khandker_shahed/Documents/vibrio_anguillarum/suppli_table/aligned/*.fasta; do python msa2snp.py "$file" > "${file%.fasta}_snp.txt"; done
